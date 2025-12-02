afb204f9c9246b9816b0c72eb7ae5fbe141e873d
Caduaugusto10 <cadu.augusto10@gmail.com>
2025-12-02 13:59:42 -0300

    feat: create component gradient and added in page contact

---
+++ components/Gradient.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+
+const Gradient = ({
+  style,
+  children,
+  colors = ['#0B1426', '#1C3A56', '#2a9d8f'],
+  locations = [0, 0.4, 1],
+  start = { x: 0, y: 0 },
+  end = { x: 0, y: 1 },
+}) => {
+  return (
+    <LinearGradient
+      colors={colors}
+      locations={locations}
+      start={start}
+      end={end}
+      style={style}
+    >
+      {children}
+    </LinearGradient>
+  );
+};
+
+export default Gradient;
